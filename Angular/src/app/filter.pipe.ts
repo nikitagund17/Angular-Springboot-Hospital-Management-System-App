5b942c3d0d6bfb3fc57b2076d3ed0cb5aabe3d02
Nikita <nikitaa.gund@gmail.com>
2023-08-09 14:36:48 +0530

    Initial project commmit

---
+++ Angular/src/app/filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  transform(items: any[], searchText: string): any[] {
+    if (!items || !searchText) {
+      return items;
+    }
+
+    searchText = searchText.toLowerCase();
+
+    return items.filter((item) =>
+      item.some((value: string) => value.toLowerCase().includes(searchText))
+    );
+  }
+}
